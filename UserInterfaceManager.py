9e8a7e48db487e43b48bcfc64bb8e11ab9e32313
xinzhao26 <82443812+xinzhao26@users.noreply.github.com>
2021-08-21 11:00:29 +1000

    Initial commit

---
+++ UserInterfaceManager.py
@@ -0,0 +1,11 @@
+#
+# File: zhaxy187_game.py
+# Author: Xin Zhao
+# Student Id: 110352998
+# Email Id: zhaxy187@mymail.unisa.edu.au
+# Date: 21/08/21
+# Description: This class manages displaying of messages to the human user
+# This is my own work as defined by the University's
+# Academic Misconduct policy.
+#
+class UserInterfaceManager:
