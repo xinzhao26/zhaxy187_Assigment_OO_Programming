09a2d9305f5109f30d0655a1bb2f678a8027e8fa
xinzhao26 <82443812+xinzhao26@users.noreply.github.com>
2021-08-29 18:18:27 +1000

    Updating user manager and tests

--- checkusermanager.py
+++ checkusermanager.py
@@ -7,15 +7,15 @@ class CheckUserManager(unittest.TestCase):
                 user_manager = UserManager()
                 
                 user_manager.register_user('Alan')
-                self.assertEqual(user_manager.users[0].name,'Alan')
+                self.assertEqual(user_manager.users[2].get_name(),'Alan')
                 
                 user_manager.register_user('Alan')
-                self.assertEqual(len(user_manager.users),1)
+                self.assertEqual(len(user_manager.users),3)
 
                 user_manager.register_user('Tyler')
-                self.assertEqual(len(user_manager.users),2)
+                self.assertEqual(len(user_manager.users),4)
 
                 user_manager.register_user('Tyler')
-                self.assertEqual(len(user_manager.users),2)
+                self.assertEqual(len(user_manager.users),4)
 
 unittest.main()
--- usermanager.py
+++ usermanager.py
@@ -12,21 +12,43 @@
 from user import User
 from userinterfacemanager import UserInterfaceManager
 
+# This class manages the users, and their registration
 class UserManager:
 
-    users = [User('HAL9000'), User('VIKI')]
+    # Set up the computer users HAL9000 and VIKI
+    __users = [User('HAL9000', True), User('VIKI', True)]
 
     def __init__(self):
         self.userinterfacemanager = UserInterfaceManager()
 
+    # This methid returns the users
     def get_users(self):
-        return self.users
+        return self.__users
 
-    def register_user(self, user_name):        
-        if(any(user.name == user_name for user in self.users)):
+    # This method registers a new user with the user_name parameter
+    def register_user(self, user_name):  
+        # If the user name passed in exists, fail
+        if(any(user.get_name() == user_name for user in self.__users)):
              self.userinterfacemanager.display_message('Sorry, the name is already taken')
+        
+        # Register a new user
         else:
-             self.users.append(User(user_name))
+             self.__users.append(User(user_name))
              message = 'Welcome, '+user_name+'!'
              self.userinterfacemanager.display_message(message)
         print('')
+
+    # This method searches for a user in the list of users based on the user name
+    def find_user(self, user_name):
+        for user in self.__users:
+            if user.get_name() == user_name:
+                # If found return the user
+                return user
+
+    # This method calls on the user's increment game stats method to increment the score and number of games played
+    def increment_game_stats(self, user_name, score):
+        user = self.find_user(user_name)
+
+        # Increment game stats of users present
+        if user != None:
+            user.increment_game_stats(score)
