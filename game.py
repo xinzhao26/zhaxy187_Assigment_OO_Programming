e8322aa9c1dd0e3ffbdadb1600ef0965cc672759
xinzhao26 <82443812+xinzhao26@users.noreply.github.com>
2021-08-29 18:59:50 +1000

    Adding game and gamemanager

--- game.py
+++ game.py
@@ -1,29 +1,78 @@
+#
+# File: feedback.py
+# Author: Xin Zhao
+# Student Id: 110352998
+# Email Id: zhaxy187@mymail.unisa.edu.au
+# Date: 27/08/21
+# Description: This class represents the Game which handles user options for different paths in the game 
+# This is my own work as defined by the University's
+# Academic Misconduct policy.
+#
+
 from userinterfacemanager import UserInterfaceManager
 from usermanager import UserManager
 from playermanager import PlayerManager
 from gamemanager import GameManager
+from scoreboard import ScoreBoard
 
-
+# This class represents a Game which handles user options for different paths in the game 
 class Game():
-
-    game_option = ''
     
     def __init__(self):
-        self.userinterface_manager = UserInterfaceManager()
-        self.user_manager = UserManager()
-        self.player_manager = PlayerManager(self.user_manager)
-        self.game_manager = GameManager(self.userinterface_manager, self.user_manager, self.player_manager)
+        # User option for game r, p, s or q
+        self.__game_option = ''
 
+        # User interface manager to handle user interface functions
+        self.__user_interface_manager = UserInterfaceManager()
+
+        # User manager to handle user related operations
+        self.__user_manager = UserManager()
+
+        # Player manager to handle player related operations
+        self.__player_manager = PlayerManager(self.__user_manager)
+
+        # Denotes the score board
+        self.__score_board = ScoreBoard(self.__user_interface_manager, self.__user_manager, self.__player_manager)
+
+        # The main manager that handles game play
+        self.__game_manager = GameManager(self.__user_interface_manager, self.__user_manager, self.__player_manager, self.__score_board)
+
+    # This method when called displays a set of options for the user to choose a path in the game
     def play_game(self):
-    	while self.game_option != 'q':
-            self.game_option = self.userinterface_manager.display_game_options()
-            if self.game_option == 'r':
-            	user_name = input('What is the name of the new user?\n')
-            	self.user_manager.register_user(user_name)
-            elif self.game_option == 'p':
-            	self.game_manager.set_up_game()
-            	self.game_manager.set_up_codes()
+        try:
+            # Show options until user enters q or quit
+            while self.__game_option != 'q':
 
+                # Call on the display game options method of the user interface class to read the user option
+                self.__game_option = self.__user_interface_manager.display_game_options()
 
+                # User has selected r - register a new user into the system
+                if self.__game_option == 'r':
+                    user_name = self.__user_interface_manager.get_user_input('What is the name of the new user?\n')
+                    self.__user_manager.register_user(user_name)
 
+                # User has selected p - to play the game
+                elif self.__game_option == 'p':
+                    # Initiaite game config
+                    self.__game_manager.set_up_game()
 
+                    # Setup codes for code breakers to break
+                    self.__game_manager.set_up_codes()
+
+                    # Commence the operations where code breakers enter codes, which is then verified by the codemaker and feedback provided if necessary
+                    self.__game_manager.guess_and_compute_codes()
+
+                # User has selected s - show scoreboard
+                elif self.__game_option == 's':
+                    self.__score_board.show_score_board()
+            else:
+                # Call the gracefully exit method that exits the game when user enters q - quit
+                self.gracefully_exit_game()
+        except KeyboardInterrupt:
+            # Call the gracefully exit method that exits the game when user enters  a Command C (Ctrl C for windows)
+            self.gracefully_exit_game()
+
+    # This method resets the input type style to normal and displays the exit message
+    def gracefully_exit_game(self):
+        self.__user_interface_manager.reset_input_type()
+        self.__user_interface_manager.display_message('\nThank you for playing the World of Mastermind!')
--- gamemanager.py
+++ gamemanager.py
@@ -12,48 +12,185 @@ from player import Player
 from userinterfacemanager import UserInterfaceManager
 from usermanager import UserManager
 import re
+from random import choice
 
+# This class manages the game play for players
 class GameManager:
 
-	def __init__(self, userinterface_manager, user_manager, player_manager):
-		self.userinterface_manager = userinterface_manager
-		self.user_manager = user_manager
-		self.player_manager = player_manager
-		self.regular_expression=re.compile('^[RGBYWK]+$')
+	def __init__(self, userinterface_manager, user_manager, player_manager, score_board):
+		self.__user_interface_manager = userinterface_manager
+		self.__user_manager = user_manager
+		self.__player_manager = player_manager
+		self.__score_board = score_board
 
+		# Only allow R, G, B, Y, W and K
+		self.__regular_expression=re.compile('^[RGBYWK]+$')
+
+		self.__letters_list = ['R','G','B','Y','W','K']
+
+	# This method sets the game up by resetting all counters to 0 - players, number of players and requests for user input
 	def set_up_game(self):
-		self.player_manager.set_number_of_players(self.userinterface_manager.display_gameplay_options())
-		self.player_manager.set_up_players()
+		self.__player_manager.reset_players()
+		self.__player_manager.set_number_of_players(self.__user_interface_manager.display_gameplay_options())
+		self.__player_manager.set_up_players()
 
+
+	# This method setups the codes for code makers to make, and code breakers to break
 	def set_up_codes(self):
+		
+		# Initialise player index to 0
 		player_index = 0
-		number_of_players = self.player_manager.get_number_of_players()
 
+		# Get the total number of players
+		number_of_players = self.__player_manager.get_number_of_players()
+		
+		# Run through every player
 		while player_index < number_of_players:
 			is_valid_code_entered = False
-			
-			if player_index == number_of_players - 1:
-				player_code_maker = self.player_manager.players[player_index]
-				player_code_breaker = self.player_manager.players[0]
-			else:
-				player_code_maker = self.player_manager.players[player_index]
-				next_index = player_index + 1
-				player_code_breaker = self.player_manager.players[next_index]
+			players = self.__player_manager.get_players()
 
-			self.userinterface_manager.display_message('\n* '+player_code_maker.get_name()+'\'s turn to set the code for '+player_code_breaker.get_name()+' to break')
+			player_code_maker = players[player_index]
+			# Round robin fashion of determinining code breaker - If it's the last player, the code breaker is the first one in the list, else the next
+			if player_index == number_of_players - 1:
+				player_code_breaker = players[0]
+			else:
+				next_index = player_index + 1
+				player_code_breaker = players[next_index]
+
+					
+			self.__user_interface_manager.display_message('\n* '+player_code_maker.get_name()+'\'s turn to set the code for '+player_code_breaker.get_name()+' to break')
 
 			while is_valid_code_entered == False:
 				try:
-					code = input('Please enter the code:\n')
-					if len(code) != 4 or self.regular_expression.match(code) == None:
-						raise ValueError('')
+					# User enters an input if not a computer
+					if player_code_maker.get_is_computer() != True:
+						code = self.__user_interface_manager.get_user_input('Please enter the code:\n')
+						if len(code) != 4 or self.__regular_expression.match(code) == None:
+							raise ValueError('')
+					
+					# Generate a random code for the computer
+					else:
+						code = self.get_random_code()
 
+					# Set the code
 					player_code_maker.set_code(code)
-					self.userinterface_manager.display_message('The code is now set for '+player_code_breaker.get_name()+' to break.')
+					self.__user_interface_manager.display_message('The code is now set for '+player_code_breaker.get_name()+' to break.')
 					is_valid_code_entered = True
 				except ValueError:
-					self.userinterface_manager.display_message('Invalid code.  \nIt must be exactly four characters, each can be R, G, B, Y, W, or K.  ')
+					self.__user_interface_manager.display_message('Invalid code.  \nIt must be exactly four characters, each can be R, G, B, Y, W, or K.  ')
 				
-
 			player_index += 1
+
+
+	# This method helps code breakers guess the code set for them by the code maker, and then their scores are calculated accordingly
+	def guess_and_compute_codes(self):
+		# Initialise attempts index to 0
+		attempts_index = 0
+
+		# Get the total number of players
+		number_of_players = self.__player_manager.get_number_of_players()
+
+		# Get the total number of attempts allowed
+		total_attempts_allowed = self.__player_manager.get_number_of_total_attempts()
+
+		# Get all players
+		players = self.__player_manager.get_players()
+
+		# Play till all players have finished playing
+		while self.__player_manager.are_players_still_playing() == True:
+			# Initialise players index to 0
+			player_index = 0
+
+			# Iterate through all players
+			while player_index < number_of_players:
+
+				player_code_breaker = players[player_index]
+
+				# Play the game is player has not already finished his/her game
+				if player_code_breaker.get_playing():
+
+					current_number_of_attempts = player_code_breaker.get_number_of_attempts()
+				
+					# Round robin fashion of determinining code maker - If it's the last player, the code maker is the first one in the list, else the next
+					if player_index == number_of_players - 1:
+						player_code_maker = players[0]
+					else:
+						next_index = player_index + 1
+						player_code_maker = players[next_index]
+
+
+					# If attempts are remaining, allow the code breaker to guess the code
+					if current_number_of_attempts <= total_attempts_allowed:
+						self.__user_interface_manager.display_message('\n* '+player_code_breaker.get_name()+'\'s turn to guess the code')
+						previous_attempts = player_code_breaker.get_number_of_attempts()
+						attempts_left = total_attempts_allowed - current_number_of_attempts
+
+						self.__user_interface_manager.display_message('Previous attempts :'+str(previous_attempts))
+
+						# Display old feedback to the code breaker, if present
+						if len(player_code_breaker.get_feedback_list()) > 0:
+							self.__user_interface_manager.display_message("==============")
+							self.__user_interface_manager.display_message("Code Feedback")
+							self.__user_interface_manager.display_message("==============")
+							for feedback_object in player_code_breaker.get_feedback_list():
+								self.__user_interface_manager.display_message(feedback_object.get_code_entered()+' '+" ".join(feedback_object.get_feedback_given()))
+							self.__user_interface_manager.display_message("==============")
+
+						self.__user_interface_manager.display_message('Attempts left :'+str(attempts_left))
+
+						is_valid_code_entered = False
+
+						while is_valid_code_entered == False:
+							try:
+								# User enters an input if not a computer
+								if player_code_breaker.get_is_computer() != True:
+									code = self.__user_interface_manager.get_user_input('Please enter the code:\n')
+									if len(code) != 4 or self.__regular_expression.match(code) == None:
+										raise ValueError('')
+								# Generate a random answer for the computer
+								else:
+									code = self.get_random_code()
+									self.__user_interface_manager.display_message(player_code_breaker.get_name()+'\'s guess: '+code)
+								
+								is_valid_code_entered = True
+							
+								# Incrementing attempts counter for player
+								increment = previous_attempts + 1
+								player_code_breaker.set_number_of_attempts(increment)
+
+								#Evaluating code entered
+								feedback = player_code_maker.evaluate_code(code)
+
+								# Feedback is None which means the correct code was entered
+								if feedback == None:
+									suffix = ' attempts!'
+									if player_code_breaker.get_number_of_attempts() == 1:
+										suffix = ' attempt!'
+									player_code_breaker.set_playing(False)
+									player_code_breaker.set_successful(True)
+									self.__user_interface_manager.display_message(player_code_breaker.get_name()+' broke the code in '+str(player_code_breaker.get_number_of_attempts()) +suffix)
+								
+								# Incorrect code was entered
+								else:
+									player_code_breaker.add_feedback(code, feedback)
+									self.__user_interface_manager.display_message('Feedback: '+feedback)
+
+								# Attempts exhausted and player lost
+								if attempts_left <= 1:
+									if player_code_breaker.get_playing() == True:
+										player_code_breaker.set_playing(False)
+										self.__user_interface_manager.display_message(player_code_breaker.get_name()+' failed to break the code.\n')
+
+							except ValueError:
+								self.__user_interface_manager.display_message('Invalid code.  \nIt must be exactly four characters, each can be R, G, B, Y, W, or K.  ')
+				player_index += 1
+
+		self.__user_interface_manager.display_message('\nThe game is now finished.')
+		self.__score_board.calculate_scores()
+			
+
+	# This method returns a random 4 character code of R, G, B, Y, W and K
+	def get_random_code(self):
+		return ''.join(choice(self.__letters_list) for i in range(4))
+
 
